7edb6df78c3b1f6f1e9b8460036f3dd1b9715a55
Ben Wilcock <bwilcock@pop-os.localdomainpivotal.io>
2020-05-07 17:31:38 +0100

    added further tests

--- rsocket-server/src/test/java/io/pivotal/rsocketserver/RSocketControllerTest.java
+++ rsocket-server/src/test/java/io/pivotal/rsocketserver/RSocketControllerTest.java
@@ -20,10 +20,12 @@ import org.springframework.messaging.rsocket.RSocketStrategies;
 import org.springframework.messaging.rsocket.annotation.support.RSocketMessageHandler;
 import org.springframework.util.MimeType;
 import org.springframework.util.MimeTypeUtils;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.time.Duration;
+import java.util.concurrent.atomic.AtomicLong;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -62,17 +64,55 @@ public class RSocketControllerTest {
     public void requestGetsResponse() {
         Mono<Message> result = requester
                 .route("request-response")
-                .data(new Message("TEST","REQUEST"))
+                .data(new Message("TEST","Request"))
                 .retrieveMono(Message.class);
 
         StepVerifier
                 .create(result)
                 .consumeNextWith(message -> {
                     assertThat(message.getOrigin()).isEqualTo(RSocketController.SERVER);
-                    assertThat(message.getInteraction()).isEqualTo("Response");
+                    assertThat(message.getInteraction()).isEqualTo(RSocketController.RESPONSE);
                     assertThat(message.getIndex()).isEqualTo(0);
                 })
                 .verifyComplete();
+    }
+
+    @Test
+    public void fireAndForget() {
+        Mono<Message> result = requester
+                .route("fire-and-forget")
+                .data(new Message("TEST","Fire-And-Forget"))
+                .retrieveMono(Message.class);
+
+        StepVerifier
+                .create(result)
+                .verifyComplete();
+    }
+
+    @Test
+    public void requestGetsStream() {
+        AtomicLong index = new AtomicLong(0L);
+
+        Flux<Message> result = requester
+                .route("stream")
+                .data(new Message("TEST","Stream"))
+                .retrieveFlux(Message.class);
+
+        StepVerifier
+                .create(result)
+                .consumeNextWith(message -> {
+                    assertThat(message.getOrigin()).isEqualTo(RSocketController.SERVER);
+                    assertThat(message.getInteraction()).isEqualTo(RSocketController.STREAM);
+                    assertThat(message.getIndex()).isEqualTo(0L);
+                })
+                .expectNextCount(3)
+                .consumeNextWith(message -> {
+                    assertThat(message.getOrigin()).isEqualTo(RSocketController.SERVER);
+                    assertThat(message.getInteraction()).isEqualTo(RSocketController.STREAM);
+                    assertThat(message.getIndex()).isEqualTo(4L);
+                })
+                .thenCancel()
+                .verify();
     }
 
     @Test
