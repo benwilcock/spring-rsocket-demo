557d6fc2f6b5ee754d46ad2835a4ca4c55aaa74b
Ben Wilcock <benwilcock@gmail.com>
2020-05-15 10:10:06 +0100

    updated the project to the latest spring boot version

--- rsocket-server/src/main/java/io/pivotal/rsocketserver/RSocketServerResumptionConfig.java
+++ rsocket-server/src/main/java/io/pivotal/rsocketserver/RSocketServerResumptionConfig.java
@@ -7,22 +7,19 @@ import org.springframework.boot.rsocket.server.RSocketServerCustomizer;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-@Slf4j
+
+/**
+ * Make the socket capable of resumption.
+ * By default, the Resume Session will have a duration of 120s, a timeout of
+ * 10s, and use the In Memory (volatile, non-persistent) session store.
+ */
 @Profile("resumption")
 @Component
 public class RSocketServerResumptionConfig implements RSocketServerCustomizer {
-
-    /**
-     * Make the socket capable of resumption.
-     * By default, the Resume Session will have a duration of 120s, a timeout of
-     * 10s, and use the In Memory (volatile, non-persistent) session store.
-     *
-     * @param rSocketServer
-     * @return
-     */
 
     @Override
     public void customize(RSocketServer rSocketServer) {
         rSocketServer.resume(new Resume());
     }
+
 }
